ec4fac5a5a72b4458e29dc0d6b1182860fa7550d
F. Javier Moure Lopez <j.moure@udc.es>
2015-11-06 18:26:33 +0100

    Test de ServidorConPadres implementado.

---
+++ src/test/java/es/udc/fic/vvs/spoticopy/servidorTest/ServidorConPadresTest.java
@@ -0,0 +1,32 @@
+package es.udc.fic.vvs.spoticopy.servidorTest;
+
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Test;
+
+import es.udc.fic.vvs.spoticopy.contenido.Cancion;
+import es.udc.fic.vvs.spoticopy.contenido.Contenido;
+import es.udc.fic.vvs.spoticopy.servidor.ServidorConPadres;
+import es.udc.fic.vvs.spoticopy.servidor.ServidorNormal;
+
+public class ServidorConPadresTest {
+	
+	@Test
+	public void buscar() {
+		List<Contenido> listaContenidos = new ArrayList<Contenido>();
+		Cancion cancion = new Cancion("cancion1",5);
+		listaContenidos.add(cancion);
+		
+		// Servidor Padre
+		ServidorNormal servidorPadre = new ServidorNormal("SP", "tokenSN", listaContenidos);
+		
+		// Servidor con Padre
+		ServidorConPadres servidorConPadres = new ServidorConPadres("SCP", servidorPadre, "tokenSCP", null);
+		
+		assertEquals(servidorConPadres.buscar("cancion","SCP"),listaContenidos);
+	}
+	
+}
