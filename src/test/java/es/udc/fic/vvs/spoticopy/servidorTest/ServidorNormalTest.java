d29e8961b8b4d0799b9420e6c2dbd4b2172bf93d
Emma Oitavén <oitavemma@gmail.com>
2015-11-06 18:04:35 +0100

    Creada clase para hacer los test del servidor

---
+++ src/test/java/es/udc/fic/vvs/spoticopy/servidorTest/ServidorNormalTest.java
@@ -0,0 +1,14 @@
+package es.udc.fic.vvs.spoticopy.servidorTest;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ServidorNormalTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
